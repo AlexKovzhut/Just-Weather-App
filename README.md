2cad002deb644c2b48300a29323bad282bd7ac42
Alexander Kovzhut <akovzhut@icloud.com>
2021-11-15 03:52:54 +0300

    Commit new version
    UI + Delegate weather and location

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Weather-Application
+# Weather Application 🌈
+____
+
+## Test application with:
+
+### **1.** Without ~~Storyboards~~
+### **2.** Supporting Dark mode
+### **3.** Dynamic fonts
+### **4.** Delegate Pattern
+### **5.** NSURLSession with JSON
+### **6.** Architecture = MVC
+____
--- Weather Application/Resources/AppDelegate.swift
+++ Weather Application/Resources/AppDelegate.swift
@@ -11,13 +11,22 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
         window?.backgroundColor = .systemBackground
-        window?.rootViewController = ViewController()
+        
+        let navigationController = UINavigationController(rootViewController: WeatherViewController())
+        window?.rootViewController = navigationController
+        
+        // For iOS 15 - navigation bar tint color
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .systemFill
+        navigationController.navigationBar.standardAppearance = appearance;
+        navigationController.navigationBar.scrollEdgeAppearance = navigationController.navigationBar.standardAppearance
         
         return true
     }
